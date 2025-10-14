d3048797e7eddf5ce6fccdc54d896a7ca4acbcbc
Nontokozo Khoza <402308465@my.richfield.co.za>
2025-10-14 21:00:15 +0200

    Fixed MongoDB connection and server now works!

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-// server.js
-const express = require('express');
-const connectDB = require('./database');
-
-const app = express();
-
-console.log("Server is starting... 🚀");
-
+// server.js 
+const express = require('express'); 
+const connectDB = require('./database'); 
+const app = express(); 
+ 
+console.log("Server is starting..."); 
+ 
+// Connect to MongoDB 
+connectDB(); 
+ 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+    console.log("Server running on port 3000"); 
+}); 
